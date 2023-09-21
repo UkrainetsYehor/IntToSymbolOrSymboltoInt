70afd7e721a03112a0cd2987e8f6cabb63f84e56
Ehor Ukrainets <0407aezakmi@gmail.com>
2023-09-21 13:47:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+void IntToSymbolOrSymboltoInt(char sign,char symbol, int var)
+{
+	for (int i = 0; i < var; i++)
+	{
+		printf("%c", sign);
+		printf("%d", symbol);
+	}
+	printf("IntToSymbo:  %c \n", sign);
+	printf("SymboltoInt: %d \n", symbol);
+}
+
+void main()
+{
+	IntToSymbolOrSymboltoInt(67,'H', 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//SymbolToInt('H', 0);
+/*
+void SymbolToInt(char sign, int var)
+{
+	for (int i = 0; i < var; i++)
+	{
+		printf("%d", sign);
+	}
+	printf("%d \n", sign);
+}
+*/
+
